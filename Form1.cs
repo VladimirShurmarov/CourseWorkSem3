6676ae8c74a91604d0c5e7a34c132fdc8f3117fb
AAA <shturm2001@mail.ru>
2025-01-19 14:00:18 +0700

    map and figures

--- Form1.cs
+++ Form1.cs
@@ -15,23 +15,24 @@ namespace CourseWorkSem3
         public Image chessSprites;
         public int[,] map = new int[8, 8]
         {
-            {5,4,3,2,1,3,4,5 },
-            {6,6,6,6,6,6,6,6 },
+            {15,14,13,12,11,13,14,15 },
+            {16,16,16,16,16,16,16,16 },
             {0,0,0,0,0,0,0,0 },
             {0,0,0,0,0,0,0,0 },
             {0,0,0,0,0,0,0,0 },
             {0,0,0,0,0,0,0,0 },
-            {6,6,6,6,6,6,6,6 },
-            {5,4,3,2,1,3,4,5 },
+            {26,26,26,26,26,26,26,26 },
+            {25,24,23,22,21,23,24,25 },
         };
+        public Button prevButton;
+        
+
         public Form1()
         {
             InitializeComponent();
 
             chessSprites = new Bitmap("C:\\Users\\AAA\\Desktop\\source\\repos\\CourseWorkSem3\\chessSprites.png");
-            Image part = new Bitmap(50,50);
-            Graphics graph = Graphics.FromImage(part);
-            graph.DrawImage(chessSprites, new Rectangle(0, 0, 50, 50), 0, 0, 150, 150, GraphicsUnit.Pixel);
+            
             //button1.BackgroundImage = part;
 
             Init();
@@ -50,11 +51,40 @@ namespace CourseWorkSem3
                 for (int j = 0; j < 8; j++)
                 {
                     Button butt = new Button();
-                    butt.Size = new Size(66, 62);
-                    butt.Location = new Point(j * 66, i * 62);
+                    butt.Size = new Size(50, 50);
+                    butt.Location = new Point(j * 50, i * 50);
+
+                    switch (map[i,j]/10)
+                    {
+
+                        case 1:
+                            Image part = new Bitmap(50, 50);
+                            Graphics graph = Graphics.FromImage(part);
+                            graph.DrawImage(chessSprites, new Rectangle(0, 0, 50, 50), 0+150 * (map[i, j] % 10-1), 150, 150, 150, GraphicsUnit.Pixel);
+                            butt.BackgroundImage = part;
+                            break;
+                        case 2:
+                            Image part2 = new Bitmap(50, 50);
+                            Graphics graph2 = Graphics.FromImage(part2);
+                            graph2.DrawImage(chessSprites, new Rectangle(0, 0, 50, 50), 0+150 * (map[i, j] % 10-1), 0, 150, 150, GraphicsUnit.Pixel);
+                            butt.BackgroundImage = part2;
+                            break;
+                    }
+                    butt.BackColor= Color.White;
+                    butt.Click += new EventHandler(OnFigurePress);
                     this.Controls.Add(butt);
                 }
             }
+        }
+        public void OnFigurePress(object sender, EventArgs e)
+        {
+            if (prevButton != null)
+                prevButton.BackColor = Color.White;
+            Button pressedButton = sender as Button;
+            if (map[pressedButton.Location.Y / 50, pressedButton.Location.X / 50] != 0)
+                pressedButton.BackColor = Color.Red;
+
+            prevButton = pressedButton;
         }
     }
 }
